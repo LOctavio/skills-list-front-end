ae6e97f746396c0446b201de593ebb4715d95ca9
LOctavio <luis_ram1997@hotmail.com>
2022-04-06 07:12:24 -0500

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './index.css';
 import Search from './Search';
+import User from './User';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Search />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Search />} />
+        <Route exact path="/user/:slug" element={<User />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
